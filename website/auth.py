fccec626b1df1ccaf3763ab44b5cd33fc33d5d61
Ashrithiiitdm <ashrithgandhe49@email.com>
2024-04-06 20:06:47 +0530

    modified database

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,7 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+import sqlite3
 
 auth = Blueprint('auth',__name__)
 
