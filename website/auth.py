4c1e316a4e5c58555595930086cc102e8cc06a5d
veereshk20 <pandiriveereshkumar@gmail.com>
2024-04-07 01:44:18 +0530

    chnages2

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-from flask import Blueprint, render_template, request, flash, redirect, url_for, send_file, jsonify
-from .models import User
-=======
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User,Inventory,Category,Units
->>>>>>> 469719455a558c44f57dbbf13ab1f8d268512af8
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
